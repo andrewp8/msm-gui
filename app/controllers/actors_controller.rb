ae63734617dec784567677995926e20e5fadfda8
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-10 03:41:58 +0000

    finished

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,53 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    @actor = Actor.new
+    @actor.name = params.fetch("query_name")
+    @actor.dob = params.fetch("query_dob")
+    @actor.bio = params.fetch("query_bio")
+    @actor.image = params.fetch("query_image")
+
+    if @actor.name.present?
+      @actor.save
+      redirect_to("/actors", {:notice => "Actor created successfully"})
+    else
+      redirect_to("/actors", {:notice => "Actor failed to create successfully"})
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @actor = Actor.find(the_id)
+    @actor.name = params.fetch("query_name")
+    @actor.dob = params.fetch("query_dob")
+    @actor.bio = params.fetch("query_bio")
+    @actor.image = params.fetch("query_image")
+
+    if @actor.name.present?
+      @actor.save
+      redirect_to("/actors/#{@actor.id}", {:notice => "Actor updated successfully"})
+    else
+      redirect_to("/actors/#{@actor.id}", {:notice => "Actor failed to update successfully"})
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_director = Actor.find(the_id)
+    the_director.destroy
+    redirect_to("/actors", {:notice => "Actor deleted successfully"})
+  end
+
+  def max_dob
+    directors_by_dob_desc = Actor.
+      all.
+      where.not({ :dob => nil }).
+      order({ :dob => :desc })
+
+    @youngest = directors_by_dob_desc.at(0)
+
+    render({ :template => "director_templates/youngest" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
+  # ------------director--------------
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
 
@@ -10,12 +11,17 @@ Rails.application.routes.draw do
   post("/modify_director/:path_id", { :controller => "directors", :action => "update" })
   get("/delete_director/:path_id", { :controller => "directors", :action => "destroy" })
 
+  # --------------movie-----------------
   get("/movies", { :controller => "movies", :action => "index" })
   post("/insert_movie", {:controller => "movies", :action => "create"})
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/modify_movie/:path_id", {:controller => "movies", :action => "update"})
   get("/delete_movie/:path_id", {:controller => "movies", :action => "destroy"})
   
+  # ------------actor--------------
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_actor", {:controller => "actors", :action => "create"})
+  post("/modify_actor/:path_id", {:controller => "actors", :action => "update"})
+  get("/delete_actor/:path_id", {:controller => "actors", :action => "destroy"})
 end
